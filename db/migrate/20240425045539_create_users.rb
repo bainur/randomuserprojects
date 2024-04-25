1dd72b33f07b9cc32bb60ef1823b446ed7fffb7b
bainur <mbainurb@gmail.com>
2024-04-25 12:00:27 +0700

    Migration for users and daily datas

---
+++ db/migrate/20240425045539_create_users.rb
@@ -0,0 +1,13 @@
+# db/migrate/YYYYMMDDHHMMSS_create_users.rb
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :uuid, limit: 36
+      t.string :gender, limit: 6, inclusion: { in: %w[male female] }
+      t.jsonb :name
+      t.jsonb :location
+
+      t.timestamps
+    end
+  end
+end
