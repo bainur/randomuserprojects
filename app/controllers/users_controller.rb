b91bf7a7f7380cce5cb452c979d1a4f219a001db
bainur <mbainurb@gmail.com>
2024-04-26 16:34:30 +0700

    implemented search feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
 
   def index
     @users = User.order('id').page(params[:page]).per(1000) # Paginate with 1000 records per page
+    if params[:search_query].present?
+      @users = @users.where("name ->> 'first' ILIKE ? OR name ->> 'last' ILIKE ? OR name ->> 'title' ILIKE ?", "%#{params[:search_query]}%", "%#{params[:search_query]}%", "%#{params[:search_query]}%")
+    end
     liquid_template = Liquid::Template.parse(File.read('app/views/users/index.liquid'))
     rendered_template = liquid_template.render('users' => @users.map(&:attributes), )
     render html: rendered_template.html_safe
